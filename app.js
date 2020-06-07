bad362b1b69f73483c2e4dec3b921ac942680eb1
Barri <info@bdesignstudio.co.uk>
2020-06-07 12:32:32 +0100

    app

---
+++ app.js
@@ -0,0 +1,13 @@
+/* 
+Game features :
+- Guess between a min and max
+- Certain amount of guesses
+- Notify remaining guesses
+- player can play again
+
+*/
+
+// Game values
+let min = 1,
+    max = 10,
+    
