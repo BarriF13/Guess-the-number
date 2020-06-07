b8a21866e743af1ce8e0aa6990c3e9049821ecc9
Barri <info@bdesignstudio.co.uk>
2020-06-07 17:09:56 +0100

    Set up vars and message function

--- app.js
+++ app.js
@@ -8,6 +8,38 @@ Game features :
 */
 
 // Game values
-let min = 1,
+let min = 2,
     max = 10,
-    
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI elements
+
+const game = document.querySelector( 'game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+
+// Assign UI min and max
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// listen for guess input 
+
+guessBtn.addEventListener('click' ,function(){
+      let guess = parseInt(guessInput.value)
+
+if(isNaN(guess) || guess < min || guess > max ){
+  setMessage(`Please enter a number between ${min} and ${max}` , 'red');
+}
+}); 
+
+// Set message
+function setMessage(msg , color ){
+  message.style.color =  color;
+  message.textContent = msg;
+}
